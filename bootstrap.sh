0d58b87d0aa446df55d16bd0fe21c66a99fea5c1
arafatm <github@arafatm.com>
2014-01-02 16:56:56 -0600

    add more libs that might be required by ruby/rails

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,30 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+ntpdate ntp.ubuntu.com
+
 echo '---------- APT update & upgrade'
 
 apt-get update 
 apt-get dist-upgrade -q -y --force-yes 
 
-echo '---------- Install apt packages Ruby and I like'
-apt-get install -y --force-yes curl git screen vim build-essential nodejs libxml2-dev libsqlite3-dev zerofree
+echo '---------- Install packages I like'
+apt-get install -y --force-yes curl git screen vim  zerofree
+echo '---------- Install ruby dependency packages'
+apt-get install -y --force-yes make g++ build-essential
+apt-get install -y --force-yes nodejs libxml2-dev libsqlite3-dev 
+sudo apt-get install -y --force-yes zlib1g-dev libyaml-dev libssl-dev 
+sudo apt-get install -y --force-yes libgdbm-dev libreadline6-dev 
+sudo apt-get install -y --force-yes libpq-dev libffi-dev
+sudo apt-get install -y --force-yes libreadline-dev libncurses5-dev
+
+#echo '---------- Install ruby 2.1.0 stable'
+#mkdir ~/tmp
+#cd ~/tmp
+#wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.0.tar.gz
+#tar xvzf ruby-2.1.0.tar.gz
+#cd ruby-2.1.0 
+#./configure  --prefix=/usr --docdir=/usr/share/doc/ruby-2.1.0 --enable-shared && make && make install && cd && rm -rf ~/tmp/*
 
 #update-alternatives --set ruby /usr/bin/ruby1.9.1
 #update-alternatives --set gem /usr/bin/gem1.9.1
