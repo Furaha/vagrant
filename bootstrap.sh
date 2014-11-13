df6df2d2856f3ec6328147b61d51380c96adcca4
Arafat M <github@arafatm.com>
2014-11-12 20:57:15 -0600

    bootstrap rbenv and postgresql

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,61 +1,113 @@
 #!/usr/bin/env bash
 
-
 export DEBIAN_FRONTEND=noninteractive
 
-ntpdate ntp.ubuntu.com
+apt_update
 
-echo
-echo
-echo "---------- APT update & upgrade"
+install_ruby
+install_postgresql
 
-apt-get update 
-apt-get dist-upgrade -q -y --force-yes 
+apt_clean
 
-echo
-echo
-echo "---------- Install packages I like"
-apt-get install -y --force-yes curl git screen vim  zerofree
-echo
-echo
-echo "---------- Install ruby build dependency packages"
-apt-get install -y --force-yes make g++ build-essential autoconf
-apt-get install -y --force-yes nodejs libxml2-dev libsqlite3-dev 
-apt-get install -y --force-yes zlib1g zlib1g-dev libyaml-dev libssl-dev 
-apt-get install -y --force-yes libgdbm-dev libreadline6 libreadline6-dev 
-apt-get install -y --force-yes libpq-dev libffi-dev
-apt-get install -y --force-yes libreadline-dev libncurses5-dev
+msg() {
+  echo "----------"
+  echo "----------"
+  echo "---------- $1"
+}
 
-echo
-echo
-echo "---------- APT clean"
-apt-get -y autoremove
-apt-get -y clean
-apt-get autoclean -y
+apt_update() {
+  ntpdate ntp.ubuntu.com
 
-echo
-echo
-echo "---------- Install ruby 2.1.0 stable"
-mkdir ~/tmp
-cd ~/tmp
+  msg "APT update & upgrade"
 
-if [ ! "$(expr substr "$(ruby -v)" 1 10)" == "ruby 2.1.0" ]; then
-  wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.0.tar.gz
-  tar xvzf ruby-2.1.0.tar.gz
-  cd ruby-2.1.0 
-  ./configure  --prefix=/usr --docdir=/usr/share/doc/ruby-2.1.0 --enable-shared && make && make install 
-else
-  echo "ruby 2.1.0 already installed"
-fi
+  apt-get update 
+  apt-get dist-upgrade -q -y --force-yes 
 
-echo
-echo
-echo "---------- Clean up ruby build tmp dir"
-cd && rm -rf ~/tmp/*
+  msg "Install packages I like"
+  apt-get install -y --force-yes curl git screen vim  zerofree
 
+  msg "Install ruby build dependency packages"
+  apt-get install -y --force-yes make g++ build-essential autoconf
+  apt-get install -y --force-yes nodejs libxml2-dev libsqlite3-dev 
+  apt-get install -y --force-yes zlib1g zlib1g-dev libyaml-dev libssl-dev 
+  apt-get install -y --force-yes libgdbm-dev libreadline6 libreadline6-dev 
+  apt-get install -y --force-yes libpq-dev libffi-dev
+  apt-get install -y --force-yes libreadline-dev libncurses5-dev
 
-echo
-echo
-echo "---------- Install rails gem"
-gem install rails --no-ri --no-rdoc
+}
+apt_clean() {
+  msg "APT clean"
+  apt-get -y autoremove
+  apt-get -y clean
+  apt-get autoclean -y
 
+  msg "Clean up ruby build tmp dir"
+  cd && rm -rf ~/tmp/*
+
+}
+
+install_ruby() {
+  if ! command -v rbenv >/dev/null 2>&1; then
+    msg "installing rbenv"
+
+    # node.js (needed for rails_
+    if [[ ! -f /etc/apt/sources.list.d/chris-lea* ]]; then 
+      sudo add-apt-repository ppa:chris-lea/node.js
+    fi
+
+    msg "base utils"
+    sudo apt-get update
+    sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev \
+      libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev \
+      libxslt1-dev libcurl4-openssl-dev python-software-properties nodejs
+
+    msg "rbenv"
+    /usr/bin/git clone git://github.com/sstephenson/rbenv.git $HOME/.rbenv
+
+    msg "rbenv: ruby-build"
+    /usr/bin/git clone git://github.com/sstephenson/ruby-build.git \
+      $HOME/.rbenv/plugins/ruby-build
+
+    msg "rbenv: rbenv-gem-rehash"
+    git clone https://github.com/sstephenson/rbenv-gem-rehash.git \
+      $HOME/.rbenv/plugins/rbenv-gem-rehash
+
+    msg "latest ruby"
+    LATEST=`rbenv install -l | grep '^\s*2.' | grep -v dev | sort | tail -n 1`
+
+    CONFIGURE_OPTS="--disable-install-doc" rbenv install $LATEST 
+    rbenv global  $LATEST
+    rbenv rehash
+
+    source $HOME/.bashrc
+
+    msg "Install rails gem"
+    gem install rails --no-ri --no-rdoc
+  else
+    msg "rbenv already installed"
+  fi
+
+}
+
+install_postgresql() {
+
+  msg "postgresql"
+
+  if [ ! -f /etc/apt/sources.list.d/pgdg.list ]; then 
+    sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ \
+      $(lsb_release -sc)-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+    wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | \
+      sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install postgresql-common postgresql-9.3 libpq-dev
+
+    msg "enter a postgresql username"
+    read name
+
+    sudo -u postgres createuser $name --createdb --pwprompt
+
+    msg "Don't forget to set postgres password -"
+    msg "  sudo -u postgres psql"
+    msg "  postgres=# \password password"
+  fi
+}
